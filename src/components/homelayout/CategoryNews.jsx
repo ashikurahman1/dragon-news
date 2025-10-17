c8ffd7e62a7c6d73c76c9e0e049b6669fe891675
ashikurahman1 <ashikurahmantuhin@gmail.com>
2025-10-18 01:47:51 +0600

    news length

--- src/components/homelayout/CategoryNews.jsx
+++ src/components/homelayout/CategoryNews.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
   const { id } = useParams();
+  const data = useLoaderData();
+
+  const [categoryNews, setCategoryNews] = useState([]);
+
+  useEffect(() => {
+    if (id == '0') {
+      setCategoryNews(data);
+      return;
+    } else if (id == '1') {
+      const filteredNews = data.filter(
+        news => news.others.is_today_pick === true
+      );
+      setCategoryNews(filteredNews);
+    } else {
+      const filteredNews = data.filter(news => news.category_id == id);
+
+      setCategoryNews(filteredNews);
+    }
+  }, [data, id]);
   return (
     <div>
-      <p>Category News {id}</p>
+      <p>Total: {categoryNews.length} news found</p>
     </div>
   );
 };
